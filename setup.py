9347eb7c18cbcd9c1887abefe2835583e0301a6b
SeongSoo Cho <nexusz99@a2company.co.kr>
2015-03-18 02:29:55 +0900

    change setup script to using pbr. and fix some typo error.

--- setup.py
+++ setup.py
@@ -1,16 +1,7 @@
 from setuptools import setup
 
-setup(name='swiftutilization',
-      version='0.0.1',
-      description='Middleware to save transfer utilization in swift',
-      license='Apache License (2.0)',
-      author='a2company',
-      author_email='admin@a2company.co.kr',
-      packages=['swiftutilization', 'swiftutilization/middleware',
-                'swiftutilization/daemon'],
-      install_requires=['swift >= 2.2.0'],
-      entry_points={'paste.filter_factory':
-                    ['swiftutilization='
-                     'swiftutilization.middleware.utilization:filter_factory']},
-      scripts={'bin/swift-utilization-aggregator'})
+setup(
+    setup_requires=['pbr'],
+    pbr=True
+)
 
